0b4a2584187e35c7e9a44981a86a90c5fc70f5a8
rodzlen <qkqhrkxdkdy@naver.com>
2025-04-09 18:39:57 +0900

    :wrench: fix elasticsearch settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,7 +111,14 @@ DATABASES = {
     }
 }
 ELASTICSEARCH_DSL = {
-    "default": {"hosts": "localhost:9200"},  # Elasticsearch 서버 주소 및 포트
+    "default": {
+        "hosts": "localhost:9200",
+        "http_auth": ("elastic", "123456"),
+        "verify_certs": False,
+        "use_ssl": True,
+        "ssl_assert_hostname": False,
+        "ssl_show_warn": False,
+    },
 }
 
 # Password validation
