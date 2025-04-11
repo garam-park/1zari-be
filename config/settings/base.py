88af5414d1658d490d78b935c5a474906630b4a7
parkh12 <parkhunsong12@gmail.com>
2025-04-11 18:44:27 +0900

    :bug: settings 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -160,3 +160,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+APPEND_SLASH = True
+
+AUTH_USER_MODEL = "user.CommonUser"
