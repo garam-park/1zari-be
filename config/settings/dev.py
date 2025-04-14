99c4bc9ec6da905d042f27fdbbf1e2da352f3811
parkh12 <parkhunsong12@gmail.com>
2025-04-14 16:45:22 +0900

    :bug: checks.yaml수정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,6 +1,15 @@
+import os
+
 from .base import *
 
 ROOT_URLCONF = "config.urls.dev_urls"
 
-GDAL_LIBRARY_PATH = "/opt/homebrew/lib/libgdal.dylib"
-GEOS_LIBRARY_PATH = "/opt/homebrew/lib/libgeos_c.dylib"
+# 로컬과 CI 환경 구분
+if os.environ.get("CI") == "true":
+    # CI 환경에서 사용하는 경로 (Ubuntu)
+    GDAL_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgdal.so"
+    GEOS_LIBRARY_PATH = "/usr/lib/x86_64-linux-gnu/libgeos_c.so"
+else:
+    # 로컬 환경에서 사용하는 경로 (macOS)
+    GDAL_LIBRARY_PATH = "/opt/homebrew/lib/libgdal.dylib"
+    GEOS_LIBRARY_PATH = "/opt/homebrew/lib/libgeos_c.dylib"
