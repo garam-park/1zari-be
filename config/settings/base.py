0354821ba699a5a3e8263a20bdb6501790ce35df
parkh12 <parkhunsong12@gmail.com>
2025-04-29 17:42:49 +0900

    :bug: redis setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -187,3 +187,8 @@ NCP_S3_ENDPOINT = secrets["NCP_S3_ENDPOINT"]
 NCP_S3_ACCESS_KEY = secrets["NCP_S3_ACCESS_KEY"]
 NCP_S3_SECRET_KEY = secrets["NCP_S3_SECRET_KEY"]
 NCP_S3_BUCKET_NAME = secrets["NCP_S3_BUCKET_NAME"]
+
+REDIS_PORT = os.environ.get("REDIS_PORT")
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_DB = os.environ.get("REDIS_DB")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
